2e258701c21586e5384ae0609dc96ec0a706ecea
aroatdlt <aroatato@gmx.com>
2018-12-17 15:43:24 +0100

    Add principal constants

---
+++ _src/assets/js/constants.js
@@ -0,0 +1,6 @@
+'use strict';
+
+//In this file we could find most of the principal declarate constant
+
+const inputEl = document.querySelector('.input__element');
+const button = document.querySelector('.button');
